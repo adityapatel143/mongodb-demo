953c3e62025c1372abcf7b22163890af5c3aaf4c
“Aditya <adityapatel081993@gmail.com>
2020-12-30 20:18:15 +0530

    Quary date from DB

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ async function createCourse() {
         tags: ["angular", "frontend"],
         isPublished: true,
     });
-
     // Saving Data
     // This is an async process.
     // it will return a unique ID.
@@ -42,4 +41,19 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+//createCourse();
+
+// Fetching data
+async function getCourses() {
+    //const courses = await Course.find(); // This will get all the data.
+    const courses = await Course.find({
+        author: "Aditya",
+        isPublished: true,
+    })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 });
+    console.log(courses);
+}
+
+getCourses();
