0fd8f98a74c2d6d26cbecf43ebf9390f6994801e
“Aditya <adityapatel081993@gmail.com>
2020-12-30 18:05:07 +0530

    Create Schema and Model

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// COnnection to Database
 mongoose
     .connect("mongodb://localhost/playground")
     .then(() => {
@@ -8,3 +9,27 @@ mongoose
     .catch(() => {
         console.error("Error connecting Mongodb....", err);
     });
+
+// Making Schema
+/* These types of data types can be used
+    String, Number, Boolean, Date, Buffer, ObjectID, Array */
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+});
+
+// Compile schema into model Model
+// In other words creating Class
+const Course = mongoose.model("Course", courseSchema);
+
+// Saving data into database.
+// In other words creating object
+const course = new Course({
+    name: "Node.js course",
+    author: "Aditya",
+    tags: ["node", "backend"],
+    isPublished: true,
+});
